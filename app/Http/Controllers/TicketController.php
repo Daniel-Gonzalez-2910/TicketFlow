a9df244925b2262f6f5bb15e0b4965dc77e49483
Luis Daniel González <daniel.go.2910@gmail.com>
2026-09-17 10:24:01 -0600

    Se agregan comentarios explicativos en TicketController

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -3,17 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// importamos nuestro modelo Ticket para poder usarlo en nuestro controlador
 use App\Models\Ticket;
 
 class TicketController extends Controller
 {
     public function index() //La función se llama tal cual index por convencion de Laravel, para que sepa que es la función principal de la ruta
     {
-        return view('tickets.index');
+        $tickets = Ticket::all(); //Se obtiene todos los tickets de la base de datos y se guarda en la variable $tickets
+        //Adicional, este return view se localiza en la carpeta resources/views/tickets/index.blade.php, que es donde se encuentra la vista que se va a mostrar al usuario
+        //return view('tickets.index');
+        return $tickets; 
     }
 
     public function create() //La función se llama tal cual create por convencion de Laravel, para que sepa que es la función principal de la ruta
     {
+        //Adicional, este return view se localiza en la carpeta resources/views/tickets/crear.blade.php, que es donde se encuentra la vista que se va a mostrar al usuario
         return view('tickets.crear');
     }
 }
